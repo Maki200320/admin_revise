d5991dd0d47552e680dfbf3b95d1bdd4e192226c
Maki200320 <clave.johnmark20@gmail.com>
2025-02-21 22:59:26 +0800

    Delete Action has been added...

--- src/AdminDashboard.js
+++ src/AdminDashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { db } from "./components/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import "./admin.css";
 
 const AdminDashboard = () => {
@@ -30,6 +30,11 @@ const AdminDashboard = () => {
     emp.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+   const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "employees", id));
+    setEmployees(employees.filter(emp => emp.id !== id));
+  };
+
   return (
     <div className="admin-container">
       <h1>Admin Dashboard</h1>
@@ -50,6 +55,7 @@ const AdminDashboard = () => {
             <th>Date</th>
             <th>Time In</th>
             <th>Time Out</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -60,6 +66,7 @@ const AdminDashboard = () => {
                 <td>{emp.date || "N/A"}</td>
                 <td>{emp.timeIn}</td>
                 <td>{emp.timeOut || "Not yet"}</td>
+                <td><button onClick={() => handleDelete(emp.id)} className="btn delete">Delete</button></td>
               </tr>
             ))
           ) : (
